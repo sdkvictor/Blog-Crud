6c2ec0424a89b97666cf2d0359f9b343fac74d4a
Victor <sdkvictor@github.com>
2020-01-24 18:28:04 -0600

    post and gets

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 let express = require('express');
 let bodyParser = require('body-parser');
-let jsonParser = bodyParser.json();n
+let jsonParser = bodyParser.json();
 let app = express();
+let uuid = require('uuid/v4')
 
 app.use(express.static('public'));
 
@@ -10,7 +11,7 @@ app.listen(8080, function(){
 })
 
 let comentarios = [{
-    id: uuid.v4(),
+    id: uuid(),
     titulo: "Hola",
     contenido: "me llamo Carlitos",
     autor: "Carlos",
@@ -25,8 +26,8 @@ app.get('/blog-api/comentarios', (req,res) => {
 });
 
 app.get('/blog-api/comentarios-por-autor', (req,res) => {
-    let autor = res.query.autor;
-    if(autor = undefined){
+    let autor = req.query.autor;
+    if(autor == undefined){
         res.statusMessage = "No se ha proporcionado un autor correctamente";
         return res.status(406).send();
     }
@@ -45,14 +46,16 @@ app.get('/blog-api/comentarios-por-autor', (req,res) => {
 });
 
 app.post('/blog-api/nuevo-comentario', jsonParser, (req,res) => {
-    
-    if(req.body.autor==""||req.body.contenido==""||req.body.titulo==""){
+    if(req.body.autor==undefined||req.body.contenido==undefined||req.body.titulo==undefined){
         res.statusMessage = "No se han recibido todos los parametros";
         return res.status(406).send();
     }
     else{
-        comentarios.push({autor:req.body.autor, titulo:req.body.titulo, contenido: req.body.contenido, date: new Date(), id: uuid.v4()});
+        comentarios.push({autor:req.body.autor, titulo:req.body.titulo, contenido: req.body.contenido, date: new Date(), id: uuid()});
         res.statusMessage = "Comentario agregado exitosamente";
         return res.status(201).send();
     }
-})
+});
+
+
+
